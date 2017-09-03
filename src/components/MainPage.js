bc72eda5de4ee8109842f356b116a5a9ab396e33
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2017-09-03 09:26:26 -0400

    finished field components

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -10,17 +10,6 @@ class MainPage extends Component {
     this.handleLogin = this.handleLogin.bind(this);
     this.handleRegister = this.handleRegister.bind(this);
 
-    this.handleLoginEmailChange = this.handleLoginEmailChange.bind(this);
-    this.handleLoginPwdChange = this.handleLoginPwdChange.bind(this);
-
-    this.handleUserNameChange = this.handleUserNameChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePwdChange = this.handlePwdChange.bind(this);
-    this.handleNameChange = this.handleNameChange.bind(this);
-
-    this.handleLogin = this.handleLogin.bind(this);
-    this.handleRegister = this.handleRegister.bind(this);
-
   }
 
   handleLogin(event) {
@@ -30,33 +19,6 @@ class MainPage extends Component {
     event.preventDefault();
   }
 
-  handleLoginEmailChange(event) {
-    this.setState({value: event.target.value});
-    var email = event.target.value;
-
-    if(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)){
-      this.props.msg = "Correct email";
-    } else {
-      this.props.msg = "nothing";
-    }
-  }
-  handleLoginPwdChange(event) {
-    this.setState({value: event.target.value});
-    console.log(this.state);
-  }
-  handleUserNameChange(event) {
-    this.setState({value: event.target.value});
-  }
-  handleEmailChange(event) {
-    this.setState({value: event.target.value});
-  }
-  handlePwdChange(event) {
-    this.setState({value: event.target.value});
-  }
-  handleNameChange(event) {
-    this.setState({value: event.target.value});
-  }
-
   render() {
     return (
       <div id="forms">
@@ -64,12 +26,8 @@ class MainPage extends Component {
           <h1>Login</h1>
           <h4>Login with your Email, Facebook, Google+ or Twitter accounts</h4>
           <form onSubmit={this.handleLogin}>
-            <Field name="login-username" displayname="Username" example="bob@gmail.com"/>
-
-            <label>Password: </label>
-            <input type="password" onChange={this.handleLoginPwdChange} />
-            <p className="login-pass-error"><i></i></p>
-
+            <Field name="email" type="text" displayname="Email" example="bob@gmail.com"/>
+            <Field name="password" type="password" displayname="Password" example=""/>
             <input type="submit" value="Login" />
           </form>
         </div>
@@ -78,25 +36,12 @@ class MainPage extends Component {
           <h4>Basic information first. Then you can update your profile further later</h4>
           <p><i>You have to be 18+ to use this app</i></p>
           <form onSubmit={this.handleRegister}>
-            <label>Username:</label>
-            <input type="text" onChange={this.handleUserNameChange} placeholder="Bob94" />
-            <p className="user-error"><i>This is what your potential employers will see</i></p>
-
-            <label>Email:</label>
-            <input type="text" onChange={this.handleEmailChange} placeholder="bob@gmail.com" />
-            <p className="email-error"><i>Use this to login!</i></p>
-
-            <label>Password:</label>
-            <input type="text" onChange={this.handlePwdChange} placeholder="" />
-            <p className="pwd-error"><i>Must be at least 8 alpha-numeric characters with at least 1 number and 1 uppercase letter</i></p>
-
-            <label>Firstname:</label>
-            <input type="text" onChange={this.handleNameChange} placeholder="Bob" />
-            <p className="first-error"><i></i></p>
-
-            <label>Lastname:</label>
-            <input type="text" onChange={this.handleNameChange} placeholder="Smith" />
-            <p className="last-error"><i></i></p>
+            <Field name="username" type="text" displayname="Username" example="Bob94" />
+            <Field name="email" type="text" displayname="Email" example="bob@gmail.com"/>
+            <Field name="password" type="password" displayname="Password" example=""/>
+            <Field name="firstname" type="text" displayname="Firstname" example="Bob"/>
+            <Field name="lastname" type="text" displayname="Lastname" example="Smith"/>
+            <Field type="birthday" />
 
             <input type="submit" value="Register" />
           </form>
@@ -110,6 +55,8 @@ class Field extends React.Component {
   constructor(props){
     super(props);
     this.handleChange = this.handleChange.bind(this);
+
+    this.state = {msg: "", valid: false};
   }
 
   handleChange(event){
@@ -117,24 +64,73 @@ class Field extends React.Component {
 
     var inp = event.target.value;
 
-    if(this.props.name == "login-username"){
+    if(this.props.name == "email"){
       if(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(inp)){
-        this.props.msg = "Valid email.";
+        this.setState({msg: "", valid: true})
       } else {
-        this.props.msg = "Sorry! Invalid email.";
+        this.setState({msg: "Sorry! Invalid Email.", valid: false})
+      }
+    } else if(this.props.name == "password") {
+      if(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(inp)){
+        this.setState({msg: "", valid: true})
+      } else {
+        this.setState({msg: "Sorry! Your password should be at least 8 alpha-numeric characters with at least 1 number, 1 uppercase and 1 lowercase letter.", valid: false})
+      }
+    } else if(this.props.name == "username"){
+      if(inp.length > 5){
+        this.setState({msg: "", valid: true})
+      } else {
+        this.setState({msg: "Sorry! Your username should be greater than 5 characters", valid: false})
+      }
+    } else {
+      if(inp.length > 1 && inp == inp.charAt(0).toUpperCase()){
+        this.setState({msg: "", valid: true})
+      } else {
+        this.setState({msg: "Sorry! Invalid Firstname or Lastname", valid: false})
       }
     }
 
   }
 
   render(){
-    return(
-      <div className="field-{this.props.name}">
-        <label>{this.props.displayname}: </label>
-        <input type="text" onChange={this.handleChange} placeholder={this.props.example} />
-        <p className="{this.props.name}-error"><i>{this.props.msg}</i></p>
-      </div>
-    )
+
+    if(this.props.type == "text" || this.props.type == "password"){
+      //input
+      return(
+        <div className={"field-" + this.props.name}>
+          <label>{this.props.displayname}: </label>
+          <input className={this.state.valid ? "valid" : "invalid"} type={this.props.type} onChange={this.handleChange} placeholder={this.props.example} />
+          <p className={this.props.name + "-error"}><i>{this.state.msg}</i></p>
+        </div>
+      )
+    } else if(this.props.type == "birthday") {
+      //birthday
+      return(
+        <div className="field-birth">
+          <label>Birthday: </label>
+          <input className="day" type="text" placeholder="Day" maxlength="2"/>
+          <select className="month">
+            <option className="def" value="0">Please Select:</option>
+            <option className="jan" value="1">January</option>
+            <option className="feb" value="2">Febuary</option>
+            <option className="mar" value="3">March</option>
+            <option className="apr" value="4">April</option>
+            <option className="may" value="5">May</option>
+            <option className="jun" value="6">June</option>
+            <option className="jul" value="7">July</option>
+            <option className="aug" value="8">August</option>
+            <option className="sep" value="9">September</option>
+            <option className="oct" value="10">October</option>
+            <option className="nov" value="11">November</option>
+            <option className="dec" value="12">December</option>
+          </select>
+          <input className="year" type="text" placeholder="Year" maxlength="4" />
+        </div>
+
+      )
+    } else {
+      //more to come
+    }
   }
 }
 
