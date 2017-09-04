8e9b513529d65f726fba3000aeac7e6b8568fd9c
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2017-09-03 22:10:09 -0400

    fixed submit bug

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -19,33 +19,15 @@ class MainPage extends Component {
     event.preventDefault();
   }
 
+  handleValid(index){
+    console.log("An event just changed!")
+  }
+
   render() {
     return (
       <div id="forms">
-        <div className="login-form">
-          <h1>Login</h1>
-          <h4>Login with your Email, Facebook, Google+ or Twitter accounts</h4>
-          <form onSubmit={this.handleLogin}>
-            <Field name="email" type="text" displayname="Email" example="bob@gmail.com"/>
-            <Field name="password" type="password" displayname="Password" example=""/>
-            <input type="submit" value="Login" />
-          </form>
-        </div>
-        <div className="register-form">
-          <h1>Register</h1>
-          <h4>Basic information first. Then you can update your profile further later</h4>
-          <p><i>You have to be 18+ to use this app</i></p>
-          <form onSubmit={this.handleRegister}>
-            <Field name="username" type="text" displayname="Username" example="Bob94" />
-            <Field name="email" type="text" displayname="Email" example="bob@gmail.com"/>
-            <Field name="password" type="password" displayname="Password" example=""/>
-            <Field name="firstname" type="text" displayname="Firstname" example="Bob"/>
-            <Field name="lastname" type="text" displayname="Lastname" example="Smith"/>
-            <Field type="birthday" />
-
-            <input type="submit" value="Register" />
-          </form>
-        </div>
+        <Form name="login" />
+        <Form name="register" />
       </div>
     );
   }
@@ -56,38 +38,44 @@ class Field extends React.Component {
     super(props);
     this.handleChange = this.handleChange.bind(this);
 
+    //this.props.isValid(this);
     this.state = {msg: "", valid: false};
   }
 
   handleChange(event){
-    event.preventDefault();
 
     var inp = event.target.value;
 
     if(this.props.name == "email"){
       if(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(inp)){
         this.setState({msg: "", valid: true})
+        this.props.isValid(this);
       } else {
         this.setState({msg: "Sorry! Invalid Email.", valid: false})
       }
     } else if(this.props.name == "password") {
       if(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(inp)){
         this.setState({msg: "", valid: true})
+        this.props.isValid(this);
       } else {
-        this.setState({msg: "Sorry! Your password should be at least 8 alpha-numeric characters with at least 1 number, 1 uppercase and 1 lowercase letter.", valid: false})
+        this.setState({msg: "Sorry! Your password should be at least 8 alpha-numeric characters with 1 number, 1 uppercase and 1 lowercase letter.", valid: false})
       }
     } else if(this.props.name == "username"){
       if(inp.length > 5){
         this.setState({msg: "", valid: true})
+        this.props.isValid(this);
       } else {
         this.setState({msg: "Sorry! Your username should be greater than 5 characters", valid: false})
       }
-    } else {
+    } else if(this.props.name == "firstname" || this.props.name == "lastname") {
       if(inp.length > 1 && inp == inp.charAt(0).toUpperCase()){
         this.setState({msg: "", valid: true})
+        this.props.isValid(this);
       } else {
         this.setState({msg: "Sorry! Invalid Firstname or Lastname", valid: false})
       }
+    } else {
+      //more to come later
     }
 
   }
@@ -105,31 +93,140 @@ class Field extends React.Component {
       )
     } else if(this.props.type == "birthday") {
       //birthday
+      const day = <input className="day" type="text" placeholder="Day" maxLength="2"/>;
+      const month = <select className="month">
+        <option className="def" value="0">Month:</option>
+        <option className="jan" value="1">January</option>
+        <option className="feb" value="2">Febuary</option>
+        <option className="mar" value="3">March</option>
+        <option className="apr" value="4">April</option>
+        <option className="may" value="5">May</option>
+        <option className="jun" value="6">June</option>
+        <option className="jul" value="7">July</option>
+        <option className="aug" value="8">August</option>
+        <option className="sep" value="9">September</option>
+        <option className="oct" value="10">October</option>
+        <option className="nov" value="11">November</option>
+        <option className="dec" value="12">December</option>
+      </select>;
+      const year = <input className="year" type="text" placeholder="Year" maxLength="4" />;
+
+      let birthday = [];
+      if(this.props.want.includes("day")){
+        birthday.push(day);
+      }
+      if(this.props.want.includes("month")){
+        birthday.push(month);
+      }
+      if(this.props.want.includes("year")){
+        birthday.push(year);
+      }
+
       return(
-        <div className="field-birth">
+        <div className={"field-" + this.props.name}>
           <label>Birthday: </label>
-          <input className="day" type="text" placeholder="Day" maxlength="2"/>
-          <select className="month">
-            <option className="def" value="0">Please Select:</option>
-            <option className="jan" value="1">January</option>
-            <option className="feb" value="2">Febuary</option>
-            <option className="mar" value="3">March</option>
-            <option className="apr" value="4">April</option>
-            <option className="may" value="5">May</option>
-            <option className="jun" value="6">June</option>
-            <option className="jul" value="7">July</option>
-            <option className="aug" value="8">August</option>
-            <option className="sep" value="9">September</option>
-            <option className="oct" value="10">October</option>
-            <option className="nov" value="11">November</option>
-            <option className="dec" value="12">December</option>
-          </select>
-          <input className="year" type="text" placeholder="Year" maxlength="4" />
+          {birthday}
         </div>
 
       )
     } else {
       //more to come
+    }
+  }
+}
+
+class Form extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {validFields: []};
+    this.addValidField = this.addValidField.bind(this);
+    this.fieldExists = this.fieldExists.bind(this);
+    this.handleRegister = this.handleRegister.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  fieldExists(currentFields, field){
+    for(var i = 0; i < currentFields.length; i++){
+      if(currentFields[i] == field){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  addValidField(field){
+    var currentFields = this.state.validFields;
+    //push only once
+    if(!this.fieldExists(currentFields, field)){
+        currentFields.push(field);
+        this.setState({validFields: currentFields});
+    }
+  }
+  handleRegister(event){
+    event.preventDefault();
+    var input_username = event.target.elements.item(0);
+    var input_email = event.target.elements.item(1);
+    var input_pwd = event.target.elements.item(2);
+    var input_firstname = event.target.elements.item(3);
+    var input_lastname = event.target.elements.item(4);
+    var input_day = event.target.elements.item(5);
+    var input_month = event.target.elements.item(6);
+    var input_year = event.target.elements.item(7);
+
+    if(this.state.length == 5){
+      //check birthday emptiness
+
+
+      //check age
+
+
+      //all good, send to the database
+    } else {
+      console.log("woops");
+    }
+  }
+  handleLogin(event){
+    event.preventDefault();
+    if(this.state.length == 2){
+      //all logged in
+      console.log("logged in");
+    } else {
+      console.log("woops");
+    }
+  }
+
+  render(){
+    if(this.props.name == "register"){
+      return(
+        <div className={this.props.name + "-form"}>
+          <h1>Register</h1>
+          <h4>Basic information first. Then you can update your profile later</h4>
+          <p><i>You have to be 18+ to use this app</i></p>
+          <form onSubmit={this.handleRegister}>
+            <Field name="username" type="text" displayname="Username" example="Bob94" isValid={this.addValidField} />
+            <Field name="email" type="text" displayname="Email" example="bob@gmail.com" isValid={this.addValidField}/>
+            <Field name="password" type="password" displayname="Password" example="" isValid={this.addValidField}/>
+            <Field name="firstname" type="text" displayname="Firstname" example="Bob" isValid={this.addValidField}/>
+            <Field name="lastname" type="text" displayname="Lastname" example="Smith" isValid={this.addValidField}/>
+            <Field name="birthday" type="birthday" want={["day", "month", "year"]} />
+
+            <input type="submit" value="Register" />
+          </form>
+        </div>
+      )
+    } else {
+      //login
+      return(
+        <div className={this.props.name + "-form"}>
+          <h1>Login</h1>
+          <h4>Login with your Email, Facebook, Google+ or Twitter accounts</h4>
+          <form onSubmit={this.handleLogin}>
+            <Field name="email"type="text" displayname="Email" example="bob@gmail.com" isValid={this.addValidField} />
+            <Field name="password" type="password" displayname="Password" example="" isValid={this.addValidField}/>
+            <input type="submit" value="Login" />
+          </form>
+        </div>
+      )
     }
   }
 }
